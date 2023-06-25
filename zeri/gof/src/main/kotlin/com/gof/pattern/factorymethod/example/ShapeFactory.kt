1fcab7e373945a31ce61a06c3a9b909bc001dc55
Slowth-KIM <rt.slowth@gmail.com>
2023-06-25 10:25:35 +0900

    feat(Factory-Method): 팩토리 메소드 패턴 관련 예제
    [코드리뷰 대응]
    - SpringbootTest 제거 후 단위 테스트로 변경
    - createShip companion object 로 변경

--- zeri/gof/src/main/kotlin/com/gof/pattern/factorymethod/example/ShapeFactory.kt
+++ zeri/gof/src/main/kotlin/com/gof/pattern/factorymethod/example/ShapeFactory.kt
@@ -1,12 +1,14 @@
 package com.gof.pattern.factorymethod.example
 
 class ShapeFactory {
-    fun createShape(shapeType: ShapeType, color: ShapeColor): Shape {
-        return when (shapeType) {
-            ShapeType.CIRCLE -> Circle(color)
-            ShapeType.TRIANGLE -> Triangle(color)
-            ShapeType.SQUARE -> Square(color)
-            else -> throw IllegalArgumentException("Invalid shape type")
+    companion object {
+        fun createShape(shapeType: ShapeType, color: ShapeColor): Shape {
+            return when (shapeType) {
+                ShapeType.CIRCLE -> Circle(color)
+                ShapeType.TRIANGLE -> Triangle(color)
+                ShapeType.SQUARE -> Square(color)
+                else -> throw IllegalArgumentException("Invalid shape type")
+            }
         }
     }
 }
--- zeri/gof/src/test/kotlin/com/gof/pattern/factorymethod/example/ShapeFactoryTest.kt
+++ zeri/gof/src/test/kotlin/com/gof/pattern/factorymethod/example/ShapeFactoryTest.kt
@@ -3,24 +3,24 @@ package com.gof.pattern.factorymethod.example
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class ShapeFactoryTest {
     @Test
     @DisplayName("ShapeFactory 가 정상적으로 작동한다")
     fun shapeFactoryTest() {
-        // given
-        val factory = ShapeFactory()
-
-        // when
-        val circle = factory.createShape(ShapeType.CIRCLE, ShapeColor.RED)
-        val square = factory.createShape(ShapeType.SQUARE, ShapeColor.BLUE)
-        val triangle = factory.createShape(ShapeType.TRIANGLE, ShapeColor.YELLOW)
+        // given && when
+        val circle = ShapeFactory.createShape(ShapeType.CIRCLE, ShapeColor.RED)
+        val square = ShapeFactory.createShape(ShapeType.SQUARE, ShapeColor.BLUE)
+        val triangle = ShapeFactory.createShape(ShapeType.TRIANGLE, ShapeColor.YELLOW)
 
         // then
         assertTrue(circle is Circle)
+        assertTrue(circle.color == ShapeColor.RED)
+
         assertTrue(square is Square)
+        assertTrue(square.color == ShapeColor.BLUE)
+
         assertTrue(triangle is Triangle)
+        assertTrue(triangle.color == ShapeColor.YELLOW)
     }
 }
