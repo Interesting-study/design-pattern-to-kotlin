2ccd4b6af006050d6b6f03daa5fad7e387ad7f7a
im-Lily <dmsruddlvhs2013@gmail.com>
2023-06-25 22:22:01 +0900

    fix(abstract-factory): ShipInventoryTest 수정

--- ria/gof/src/test/kotlin/com/gof/pattern/abstractFactory/after/ShipInventoryTest.kt
+++ ria/gof/src/test/kotlin/com/gof/pattern/abstractFactory/after/ShipInventoryTest.kt
@@ -4,33 +4,30 @@ import com.gof.pattern.abstractFactory.before.WhiteAnchor
 import com.gof.pattern.abstractFactory.before.WhiteWheel
 import com.gof.pattern.factory.Ship
 import com.gof.pattern.factory.ShipFactory
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class ShipInventoryTest {
 
     @Test
-    @DisplayName("whiteShipPartsFactory 타입의 배를 만드는 테스트")
-    fun whiteShipPartsFactory() {
+    fun `whiteShipPartsFactory 타입의 배를 만드는 테스트`() {
         // given
         val shipFactory: ShipFactory = WhiteShipFactory(WhiteShipPartsFactory())
         // when
         val ship: Ship = shipFactory.createShip()
         // then
-        assertThat(ship.anchor!!::class).isEqualTo(WhiteAnchor::class)
-        assertThat(ship.wheel!!::class).isEqualTo(WhiteWheel::class)
+        assertTrue(ship.anchor is WhiteAnchor)
+        assertTrue(ship.wheel is WhiteWheel)
     }
 
     @Test
-    @DisplayName("whiteShipPartsProFactory 타입의 배를 만드는 테스트")
-    fun whiteShipPartsProFactory() {
+    fun `whiteShipPartsProFactory 타입의 배를 만드는 테스트`() {
         // given
         val shipFactory: ShipFactory = WhiteShipFactory(WhitePartsProFactory())
         // when
         val ship: Ship = shipFactory.createShip()
         // then
-        assertThat(ship.anchor!!::class).isEqualTo(WhiteAnchorPro::class)
-        assertThat(ship.wheel!!::class).isEqualTo(WhiteWheelPro::class)
+        assertTrue(ship.anchor is WhiteAnchorPro)
+        assertTrue(ship.wheel is WhiteWheelPro)
     }
 }
