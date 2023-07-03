0510cce22cc26336a5849e43838c2adc4fc3ab01
Slowth-KIM <rt.slowth@gmail.com>
2023-07-03 21:57:51 +0900

    feat(Abstract Factory): 추상 팩토리 메소드 패턴 예제
    - 코드리뷰 대응(exception 으로 처리) 및 테스트 코드 수정

--- zeri/gof/src/main/kotlin/com/gof/pattern/abstractfactory/example/FruitFactory.kt
+++ zeri/gof/src/main/kotlin/com/gof/pattern/abstractfactory/example/FruitFactory.kt
@@ -4,15 +4,13 @@ import java.util.*
 
 class FruitFactory {
     companion object {
-        fun createFruit(fruitType: String): Fruit? {
-            return try {
-                when (FruitType.valueOf(fruitType.uppercase(Locale.getDefault()))) {
-                    FruitType.APPLE -> Apple()
-                    FruitType.BANANA -> Banana()
-                }
-            } catch (ex: IllegalArgumentException) {
-                null
+        fun createFruit(fruitType: String): Fruit {
+            return when (FruitType.valueOf(fruitType.uppercase(Locale.getDefault()))) {
+                FruitType.APPLE -> Apple()
+                FruitType.BANANA -> Banana()
+                else -> throw IllegalArgumentException("Invalid fruit type: $fruitType")
             }
         }
     }
 }
+
--- zeri/gof/src/test/kotlin/com/gof/pattern/abstractfactory/example/FruitFactoryTest.kt
+++ zeri/gof/src/test/kotlin/com/gof/pattern/abstractfactory/example/FruitFactoryTest.kt
@@ -1,27 +1,27 @@
 package com.gof.pattern.abstractfactory.example
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class FruitFactoryTest {
     @Test
     @DisplayName("추상팩토리패턴 예제가 정상적으로 작동한다.")
-    fun abstractFactoryMethodTest() {
+    fun abstractFactoryMethodTest()  {
         // given && when
-        val apple: Fruit? = FruitFactory.createFruit("APPLE")
-        val banana: Fruit? = FruitFactory.createFruit("BANANA")
-        val orange: Fruit? = FruitFactory.createFruit("ORANGE")
+        val apple: Fruit = FruitFactory.createFruit("APPLE")
+        val banana: Fruit = FruitFactory.createFruit("BANANA")
+
+        assertThrows<IllegalArgumentException> {
+            FruitFactory.createFruit("ORANGE")
+        }
 
         // then
         assertTrue(apple is Apple)
-        assertEquals(apple?.getName(), "사과")
+        assertEquals(apple.getName(), "사과")
 
         assertTrue(banana is Banana)
-        assertEquals(banana?.getName(), "바나나")
-
-        assertTrue(orange == null)
-        assertEquals(orange?.getName(), null)
+        assertEquals(banana.getName(), "바나나")
     }
 }
