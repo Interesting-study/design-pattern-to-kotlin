0b14148ba84cbf15cc4e1f177f5f09fe7245a7c5
DOHEE KIM <rt.slowth@gmail.com>
2023-07-05 20:38:58 +0900

    feat(Abstract Factory): 추상 팩토리 메소드 패턴 예제 (#14)
    * feat(Abstract Factory): 추상 팩토리 메소드 패턴
    
    - 추상 팩토리 패턴 1부 ~ 4부
    - DisplayName 변경
    - Fruit 인터페이스와 이를 기준으로 하는 Banana, Apple, FruitFactory 생성
    - 코드리뷰 대응(exception 으로 처리) 및 테스트 코드 수정
    - ktlint

---
+++ zeri/gof/src/main/kotlin/com/gof/pattern/abstractfactory/example/Banana.kt
@@ -0,0 +1,7 @@
+package com.gof.pattern.abstractfactory.example
+
+class Banana : Fruit {
+    override fun getName(): String {
+        return "바나나"
+    }
+}
---
+++ zeri/gof/src/test/kotlin/com/gof/pattern/abstractfactory/example/FruitFactoryTest.kt
@@ -0,0 +1,28 @@
+package com.gof.pattern.abstractfactory.example
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class FruitFactoryTest {
+    @Test
+    @DisplayName("추상팩토리패턴 예제가 정상적으로 작동한다.")
+    fun abstractFactoryMethodTest() {
+        // given && when
+        val apple: Fruit = FruitFactory.createFruit("APPLE")
+        val banana: Fruit = FruitFactory.createFruit("BANANA")
+
+        assertThrows<IllegalArgumentException> {
+            FruitFactory.createFruit("ORANGE")
+        }
+
+        // then
+        assertTrue(apple is Apple)
+        assertEquals(apple.getName(), "사과")
+
+        assertTrue(banana is Banana)
+        assertEquals(banana.getName(), "바나나")
+    }
+}
