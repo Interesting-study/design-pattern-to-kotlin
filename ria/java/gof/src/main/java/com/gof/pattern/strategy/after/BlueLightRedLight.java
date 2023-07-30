f1f1609fd88fcdd8cfa5abd614aff8ae3155f3d1
im-Lily <dmsruddlvhs2013@gmail.com>
2023-07-30 09:48:14 +0900

    feat(strategy): 전략패턴 자바 코드 추가

---
+++ ria/java/gof/src/main/java/com/gof/pattern/strategy/after/BlueLightRedLight.java
@@ -0,0 +1,13 @@
+package com.gof.pattern.strategy.after;
+
+// Context Class
+public class BlueLightRedLight {
+
+    public void blueLight(Speed speed) {
+        speed.blueLight();
+    }
+
+    public void redLight(Speed speed) {
+        speed.redLight();
+    }
+}
