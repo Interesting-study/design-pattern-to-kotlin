591db3c09796fa349cda927ed25aab7343d79812
eunkyung <dmsruddlvhs2013@gmail.com>
2023-08-16 20:13:36 +0900

    테스트

--- ria/java/gof/src/main/java/com/gof/pattern/bridge/after/App.java
+++ ria/java/gof/src/main/java/com/gof/pattern/bridge/after/App.java
@@ -2,7 +2,6 @@ package com.gof.pattern.bridge.after;
 
 import com.gof.pattern.bridge.before.Champion;
 
-//
 public class App {
     public static void main(String[] args) {
         Champion kda아리 = new 아리(new KDA());
